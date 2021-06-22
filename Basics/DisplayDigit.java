1757fbb4be16953f4f59f4b1d42c440d2426e400
srijanmalhotra2711 <srijan.malhotra2711@gmail.com>
2021-06-22 12:42:15 +0530

    22nd June 2021

---
+++ Basics/DisplayDigit.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class DisplayDigit {
+    public static void main(String[] args) {
+        int d,n,count=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Number:");
+        n = sc.nextInt();
+        int temp = n;
+        while(n>0)
+        {
+            n = n/10;
+        }
+        while(temp>0)
+        {
+            d = temp % 10;
+            System.out.println("Digit:" + d);
+            temp=temp/10;
+        }
+    }
+}

f84a49fca922510488b49fa64d070a830622427e
srijanmalhotra2711 <srijan.malhotra2711@gmail.com>
2021-06-29 17:33:35 +0530

    29th June 2021

---
+++ Array/SortSearch.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.Scanner;
+
+class SortSearch {
+    static int search(int arr[], int l, int h, int key) {
+        if (l > h)
+            return -1;
+
+        int mid = (l + h) / 2;
+        if (arr[mid] == key)
+            return mid;
+
+        if (arr[l] <= arr[mid]) {
+            if (key >= arr[l] && key <= arr[mid])
+                return search(arr, l, mid - 1, key);
+            return search(arr, mid + 1, h, key);
+        }
+
+        if (key >= arr[mid] && key <= arr[h])
+            return search(arr, mid + 1, h, key);
+
+        return search(arr, l, mid - 1, key);
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int key = sc.nextInt();
+        int arr[] = {4, 5, 6, 7, 8, 9, 1, 2, 3};
+        int n = arr.length;
+        int i = search(arr, 0, n - 1, key);
+        if (i != -1) {
+            System.out.println("Index: " + i);
+        } else {
+            System.out.println("Key not found");
+        }
+    }
+}

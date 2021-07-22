facdf69ddff3a9292afff18eb336263306d8eb8d
srijanmalhotra2711 <srijan.malhotra2711@gmail.com>
2021-07-22 16:54:54 +0530

    22nd July 2021

---
+++ Basics/removewhite.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+String removeWhiteSpaces(String input){
+	StringBuilder output = new StringBuilder();
+
+	char[] charArray = input.toCharArray();
+
+	for(char c : charArray) {
+		if (!Character.isWhitespace(c))
+			output.append(c);
+	}
+
+	return output.toString();
+}

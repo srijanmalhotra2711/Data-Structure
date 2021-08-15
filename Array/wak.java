8a9a2f889713e2d34635bf3d910cff8c973ebcc8
srijanmalhotra2711 <srijan.malhotra2711@gmail.com>
2021-08-15 22:33:04 +0530

    15th August 2021

---
+++ Array/wak.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class wak{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner sc = new Scanner(System.in);
+
+    int n1 = sc.nextInt();
+    int m1 = sc.nextInt();
+
+    int arr[][] = new int[n1][m1];
+
+    for(int i = 0; i < arr.length; i++) {
+        for (int j = 0; j < arr[0].length; j++) {
+            arr[i][j] = sc.nextInt();
+        }
+    }
+
+    for(int j = 0; j < arr[0].length; j++) {
+       if (j % 2 == 0) {
+           for(int i = 0; i < arr.length; i++) {
+               System.out.println(arr[i][j]);
+           }
+       } else {
+           for(int i = arr.length - 1; i >= 0; i--) {
+               System.out.println(arr[i][j]);
+           }
+       }
+    }
+ }
+
+}

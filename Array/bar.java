aa0e902d460af240d901d4c8ac584fe90567f55c
srijanmalhotra2711 <srijan.malhotra2711@gmail.com>
2021-08-28 17:17:20 -0400

    28th August 2021

---
+++ Array/bar.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int arr[] = new int[n];
+    for(int i = 0; i < n; i++) {
+        arr[i] = sc.nextInt();
+    }
+    int max = Integer.MIN_VALUE;
+    for(int i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    for(int floor = max; floor > 0; floor--) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= floor) {
+                System.out.print("*\t");
+            }
+            else {
+                System.out.print("\t");
+            }
+        }
+        System.out.println();
+    }
+ }
+
+}

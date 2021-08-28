7e848ca5d1c75a34c8645c7f788cefbc4a5119fd
srijanmalhotra2711 <srijan.malhotra2711@gmail.com>
2021-08-28 17:17:35 -0400

    28th August 2021

--- Array/bar.java
+++ Array/bar.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Main{
 
 public static void main(String[] args) throws Exception {
-    // write your code here
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     int arr[] = new int[n];
